dc221e93d94016999e82178a47163649a2cec524
Mehdi <mehdi.bouchbouk@student.vinci.be>
2022-12-06 17:19:28 +0100

    structure with query param

--- src/ressources/UserRoutes.py
+++ src/ressources/UserRoutes.py
@@ -2,13 +2,13 @@ import falcon
 
 
 class Users:
-    def on_get_email(self,req,resp):
+    def on_get_email(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.media = req.params['id']
+        if req.params:
+            resp.media = req.params['id']
+        else:
+            resp.media = {'Message': 'Hello my friend'}
 
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.media = {'Message': 'Hello World'}
-
-
-

579e517d58d92cf6a0b7716d3ca1c27cc92c0463
Mehdi <mehdi.bouchbouk@student.vinci.be>
2022-12-06 16:38:34 +0100

    cors enabled and multiple route for user

--- src/app.py
+++ src/app.py
@@ -1,20 +1,28 @@
 from wsgiref.simple_server import make_server
 import falcon
 
-class HelloWorldJson(object):
-    def on_get(self,req, resp):
-        resp.status = falcon.HTTP_200
-        resp.media = {'Message' : 'Hello World'}
+from ressources.UserRoutes import Users
 
-class HelloWorldText(object):
-    def on_get(self, req , resp):
+
+class HelloWorldJson:
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.media = {'Message': 'Hello World'}
+
+
+class HelloWorldText:
+    def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = "Hello World"
 
+
 if __name__ == '__main__':
-    api = falcon.App()
+    api = falcon.App(cors_enable=True)
+
     api.add_route('/json', HelloWorldJson())
     api.add_route('/text', HelloWorldText())
+    api.add_route('/users', Users())
+    api.add_route('/users/email', Users(), suffix='email')
     print("Server started")
-    with make_server('',8080,api) as httpd:
+    with make_server('', 8080, api) as httpd:
         httpd.serve_forever()

6afb20b402737c04a3d295e9570b5dfe6ade173e
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-12-09 17:31:42 +0100

    update new post schema

---
+++ src/resources/PostResource.py
@@ -0,0 +1,41 @@
+import json
+
+import falcon
+from falcon.media.validators import jsonschema
+from src.media import load_schema
+from src.utils.Authenticate import Authenticate
+from src.utils import enum
+from src.services.PostsService import PostService
+from json import dumps
+
+auth = Authenticate()
+
+
+class Posts:
+
+    def __init__(self):
+        self.postServices = PostService.getInstance()
+
+    @jsonschema.validate(load_schema("new_post"))
+    @falcon.before(auth, enum.ROLE_USER)
+    def on_post(self, req, resp):
+        # récupérer le json
+        raw_json = req.media
+
+        id_user = req.context.user['id']
+
+        id_url = None
+        if 'id_url' not in raw_json:
+            raw_json['id_url'] = None
+        id_url = raw_json['id_url']
+
+        newPost = self.postServices.createPost(id_user, id_url, raw_json['text'])
+
+        resp.status = falcon.HTTP_201
+        resp.body = dumps({
+            'id_video': newPost[0],
+            'id_url': newPost[1],
+            'state': newPost[2],
+            'date_published': newPost[3],
+            'text': newPost[4],
+        }, default=str)
---
+++ src/services/PostsService.py
@@ -0,0 +1,35 @@
+import falcon
+from src.data.db import Db
+from datetime import datetime, timezone
+
+
+class PostService:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if PostService.__instance is None:
+            PostService()
+        return PostService.__instance
+
+    def __init__(self):
+        if PostService.__instance is not None:
+            raise Exception("UserService instance already exist !!")
+        else:
+            PostService.__instance = self
+        db = Db.getInstance()
+        self.conn = db.conn
+
+    def createPost(self, id_user, id_url, text):
+        cur = self.conn.cursor()
+
+        cur.execute("INSERT INTO youshare.posts(id_user, id_url,text,date_published)"
+                    " VALUES (%s,%s,%s,%s) RETURNING id_video,id_url,state,date_published,text",
+                    [id_user, id_url, text, datetime.now(timezone.utc)])
+
+        row = cur.fetchone()
+        print("row ::: ", row)
+
+        self.conn.commit()
+        cur.close()
+        return row

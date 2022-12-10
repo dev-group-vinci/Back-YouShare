50aa48fb1b9ad8201541cee27bd84ce6f154b04f
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-12-10 15:07:08 +0100

    add readLikes in LikeResource

--- src/resources/LikeResource.py
+++ src/resources/LikeResource.py
@@ -24,3 +24,10 @@ class Likes:
 
         resp.status = falcon.HTTP_201
         resp.body = dumps(nb_like, default=int)
+
+    @falcon.before(auth, enum.ROLE_USER)
+    def on_get(self, req, resp, id_post):
+        nb_like = self.likeServices.readNbLike(id_post)
+
+        resp.status = falcon.HTTP_201
+        resp.body = dumps(nb_like, default=int)

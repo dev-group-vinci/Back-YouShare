e7882277d76cc022da3a767d475cae6511ecc0f9
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-12-11 16:52:57 +0100

    remove like & share models because useless

--- src/models/likes.py
+++
@@ -1,8 +0,0 @@
-class Like:
-    def __init__(self, id_user, id_post):
-        self.id_user = id_user
-        self.id_post = id_post
-
-    @classmethod
-    def from_tuple(cls, tuple):
-        return cls(*tuple)

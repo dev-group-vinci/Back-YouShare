e72cdf8e3362eeb6ab45636765083cafe1da5b4c
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-12-11 16:50:18 +0100

    Add Share model

---
+++ src/models/shares.py
@@ -0,0 +1,8 @@
+class Share:
+    def __init__(self, id_user, id_post):
+        self.id_user = id_user
+        self.id_post = id_post
+
+    @classmethod
+    def from_tuple(cls, tuple):
+        return cls(*tuple)

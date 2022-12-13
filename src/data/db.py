35bba211605d7838a6656fb1c7b384c09d590be0
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-12-13 14:15:09 +0100

    manage connections with pool connection

--- src/data/db.py
+++ src/data/db.py
@@ -1,47 +1,51 @@
 import threading
 from src.utils.logging import logger
+import psycopg2.pool
 import psycopg2
 import os
 
 
 class Db:
-
-    __instance = None
+    _local = threading.local()
 
     @staticmethod
     def getInstance():
-        if Db.__instance is None:
-            Db()
-        return Db.__instance
+        if not hasattr(Db._local, "instance"):
+            Db._local.instance = Db()
+        return Db._local.instance
 
     def __init__(self):
-        if Db.__instance is not None:
-            raise Exception("Db instance already exist !!")
-        else:
-            Db.__instance = self
-            self.conn = None
+        self.conn = None
+        self.pool = None
 
     def connect(self):
         try:
-            self.conn = psycopg2.connect(
-                host=os.getenv("DB_HOST"),
-                database=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASS"),
-                port=os.getenv("DB_PORT"),
-            )
+            if self.pool is None:
+                self.pool = psycopg2.pool.SimpleConnectionPool(
+                    minconn=1,
+                    maxconn=5,
+                    host=os.getenv("DB_HOST"),
+                    database=os.getenv("DB_NAME"),
+                    user=os.getenv("DB_USER"),
+                    password=os.getenv("DB_PASS"),
+                    port=os.getenv("DB_PORT"),
+                )
 
-            self.conn.autocommit = False
-
-            logger.info("Database connection done")
+                logger.info("Database connection done")
         except(Exception, psycopg2.DatabaseError) as error:
             logger.warning(error)
-            self.conn.close()
+            raise error
+
+    def getConnection(self):
+        self.conn = self.pool.getconn()
+        self.conn.autocommit = False
         return self.conn
 
     def close(self):
         try:
-            self.conn.close()
+            if self.pool and self.conn:
+                self.pool.putconn(self.conn)
+                self.conn = None
         except(Exception, psycopg2.InternalError) as error:
             self.conn = None
             logger.warning(error)

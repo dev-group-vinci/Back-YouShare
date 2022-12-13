c10e7383aca7a1e67c1069bdee34b62642a08462
Mehdi Bouchbouk <mehdi.bouchbouk@student.vinci.be>
2022-12-13 17:32:34 +0100

    parse users

--- src/utils/json.py
+++ src/utils/json.py
@@ -5,3 +5,12 @@ def datetime_to_iso_str(o):
     if isinstance(o, datetime):
         return o.isoformat()
     return o.__dict__
+
+def parseList(list):
+    newList = []
+    for element in list:
+        newList.append(element.unescape().__dict__)
+    return newList
+
+def parseElement(element):
+    return element.unescape().__dict__

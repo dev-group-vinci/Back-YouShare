3a75a5f3b48947681f76fd9130416590cb43e275
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-12-15 12:08:33 +0100

    break test to see if pipeline breaks

--- test/test_bidon.py
+++ test/test_bidon.py
@@ -3,7 +3,7 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+        self.assertEqual(False, True)  # add assertion here
 
 
 if __name__ == '__main__':
